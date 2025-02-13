a127e453419f4ea209ebc3b9e0d323aadef669da
kyuff <soren@sxl.dk>
2025-02-13 10:43:34 +0100

    Add context option

--- options.go
+++ options.go
@@ -1,12 +1,13 @@
 package anchor
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/kyuff/anchor/internal/logger"
 )
 
-type Option func(*Config)
+type Option func(cfg *Config)
 
 func WithLogger(logger Logger) Option {
 	return func(opt *Config) {
@@ -25,4 +26,16 @@ func WithSlog(log *slog.Logger) Option {
 	return WithLogger(
 		logger.NewSlog(log),
 	)
+}
+
+// WithContext runs the Anchor in the given Context. If it is
+// cancelled, the Anchor will shutdown.
+func WithContext(ctx context.Context) Option {
+	return func(cfg *Config) {
+		cfg.ctx = ctx
+	}
+}
+
+func withBackgroundContext() Option {
+	return WithContext(context.Background())
 }
