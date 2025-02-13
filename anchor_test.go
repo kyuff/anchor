896fb6d4dddcaaacad5ae65cfff53d7b5580806b
kyuff <soren@sxl.dk>
2025-02-13 16:42:53 +0100

    Add test of starting in a go routine

--- anchor_test.go
+++ anchor_test.go
@@ -2,7 +2,7 @@ package anchor_test
 
 import (
 	"context"
-	"fmt"
+	"sync"
 	"testing"
 
 	"github.com/kyuff/anchor"
@@ -10,23 +10,42 @@ import (
 )
 
 func TestAnchor(t *testing.T) {
+	var (
+		newComponent = func(name string, mods ...func(c *fullComponentMock)) *fullComponentMock {
+			c := &fullComponentMock{
+				CloseFunc: func() error {
+					return nil
+				},
+				NameFunc: func() string {
+					return name
+				},
+				SetupFunc: func(ctx context.Context) error {
+					return nil
+				},
+				StartFunc: func(ctx context.Context) error {
+					return nil
+				},
+			}
+
+			for _, mod := range mods {
+				mod(c)
+			}
+
+			return c
+		}
+	)
 	t.Run("should setup, start and close all components", func(t *testing.T) {
 		// arrange
 		var (
-			components = []*fullComponentMock{{}, {}, {}}
-			sut        = anchor.New()
+			components = []*fullComponentMock{
+				newComponent("c-0"),
+				newComponent("c-1"),
+				newComponent("c-2"),
+			}
+			sut = anchor.New()
 		)
 
-		for i, component := range components {
-			component.SetupFunc = func(ctx context.Context) error {
-				return nil
-			}
-			component.StartFunc = func(ctx context.Context) error {
-				return nil
-			}
-			component.CloseFunc = func() error { return nil }
-			component.NameFunc = func() string { return fmt.Sprintf("mock-%d", i) }
-
+		for _, component := range components {
 			sut.Add(component)
 		}
 
@@ -39,6 +58,46 @@ func TestAnchor(t *testing.T) {
 			assert.Equal(t, 1, len(component.SetupCalls()))
 			assert.Equal(t, 1, len(component.StartCalls()))
 			assert.Equal(t, 1, len(component.CloseCalls()))
+		}
+	})
+
+	t.Run("start components in go routine", func(t *testing.T) {
+		// arrange
+		var (
+			withWaitGroupOnStart = func(wg *sync.WaitGroup) func(c *fullComponentMock) {
+				return func(c *fullComponentMock) {
+					c.StartFunc = func(ctx context.Context) error {
+						wg.Done()
+						return nil
+					}
+				}
+			}
+			ctx, cancel = context.WithCancel(t.Context())
+			started     = &sync.WaitGroup{}
+			components  = []*fullComponentMock{
+				newComponent("c-0", withWaitGroupOnStart(started)),
+				newComponent("c-1", withWaitGroupOnStart(started)),
+				newComponent("c-2", withWaitGroupOnStart(started)),
+			}
+
+			sut = anchor.New(anchor.WithContext(ctx))
+		)
+
+		for _, component := range components {
+			sut.Add(component)
+			started.Add(1)
+		}
+
+		// act
+		go func() {
+			_ = sut.Run()
+		}()
+
+		// assert
+		started.Wait()
+		cancel()
+		for _, component := range components {
+			assert.Equal(t, 1, len(component.StartCalls()))
 		}
 	})
 }
